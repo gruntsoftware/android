3a48b96292866bb92ae9d32f12e687a3d75354f1
Kerry Washington <mrkerrywashington@icloud.com>
2025-01-10 16:07:13 +0000

    update partner names

--- app/src/main/java/com/breadwallet/presenter/entities/PartnerNames.kt
+++ app/src/main/java/com/breadwallet/presenter/entities/PartnerNames.kt
@@ -1,10 +1,8 @@
 package com.breadwallet.presenter.entities
 
 enum class PartnerNames(val key: String) {
-    LITEWALLETOPS("litewallet-ops"),
-    OPSALL("litewallet-ops"),
-    LITEWALLETSTART("litewallet-start"),
-    PUSHER("pusher-instance-id"),
-    PUSHERSTAGING("pusher-staging-instance-id"),
+    WALLETOPS("wallet-ops"),
+    OPSALL("wallet-ops"),
+    WALLETSTART("start-date"),
     AFDEVID("af-dev-id")
 }
