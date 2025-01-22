e3ca48f95b8d0ea4bd152163d7a4d0c5970ca1a7
Kerry Washington <mrkerrywashington@icloud.com>
2025-01-22 20:33:38 +0000

    Deleted all color scheme and logos

--- app/src/main/java/com/breadwallet/presenter/fragments/BuyTabFragment.java
+++ app/src/main/java/com/breadwallet/presenter/fragments/BuyTabFragment.java
@@ -49,7 +49,6 @@ public class BuyTabFragment extends Fragment {
 
     private List<Partner> getPartners() {
         List<Partner> partners = new ArrayList<>();
-        partners.add(new Partner(R.drawable.bitrefill_logo, R.string.BuyCenter_bitrefill_Title, R.string.BuyCenter_bitrefill_FinancialDetails, FragmentBuy.Partner.BITREFILL));
         partners.add(new Partner(R.drawable.ic_moonpay_logo, R.string.BuyCenter_moonpay_Title, R.string.BuyCenter_moonpay_FinancialDetails, FragmentBuy.Partner.MOONPAY));
         return partners;
     }
