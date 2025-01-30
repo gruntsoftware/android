2ef5b5075b9b24178993448ac9d64476c71e5558
andhikayuana <andhikayuana@gmail.com>
2025-01-31 06:46:12 +0700

    chore: [UI] add YourSeedProveItScreen

---
+++ app/src/main/java/com/brainwallet/ui/screen/yourseedproveit/YourSeedProveItViewModel.kt
@@ -0,0 +1,38 @@
+package com.brainwallet.ui.screen.yourseedproveit
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class YourSeedProveItViewModel : ViewModel() {
+
+    private val _state = MutableStateFlow(YourSeedProveItState())
+    val state: StateFlow<YourSeedProveItState> = _state.asStateFlow()
+
+    //
+    fun onEvent(event: YourSeedProveItEvent) {
+        when (event) {
+            is YourSeedProveItEvent.OnLoad -> _state.update {
+                it.copy(
+                    correctSeedWords = event.seedWords.associateWith { "" },
+                    shuffledSeedWords = event.seedWords.shuffled()
+                )
+            }
+
+            is YourSeedProveItEvent.OnDropSeedWordItem -> _state.update {
+                val correctSeedWords = it.correctSeedWords.toMutableMap().apply {
+                    this[event.expectedWord] = event.actualWord
+                }
+
+                it.copy(
+                    correctSeedWords = correctSeedWords,
+                    orderCorrected = correctSeedWords.all { (expectedWord, actualWord) -> expectedWord == actualWord }
+                )
+            }
+
+            else -> Unit
+        }
+    }
+}
