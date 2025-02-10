624a025a5397d3b2a404a68b527b030958ca7f03
andhikayuana <andhikayuana@gmail.com>
2025-02-10 14:47:40 +0700

    chore: move PinDotItem and CircleButton to com.brainwallet.ui.screen.unlock.composable

--- app/src/main/java/com/brainwallet/ui/screen/unlock/UnLockScreen.kt
+++ app/src/main/java/com/brainwallet/ui/screen/unlock/UnLockScreen.kt
@@ -4,7 +4,6 @@ package com.brainwallet.ui.screen.unlock
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,18 +15,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -47,7 +42,10 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.brainwallet.R
 import com.brainwallet.tools.util.BRConstants
+import com.brainwallet.ui.screen.unlock.composable.CircleButton
+import com.brainwallet.ui.screen.unlock.composable.PinDotItem
 
+//TODO: WIP here
 @Composable
 fun UnLockScreen(
     onEvent: (UnLockEvent) -> Unit,
@@ -193,38 +191,4 @@ fun UnLockScreen(
             )
         }
     }
-}
-
-
-@Composable
-private fun PinDotItem(
-    modifier: Modifier = Modifier,
-    checked: Boolean = false
-) {
-    Box(
-        modifier = modifier
-            .padding(horizontal = 4.dp)
-            .size(16.dp)
-            .background(if (checked) Color.White else Color.Transparent, CircleShape)
-            .border(1.dp, Color.White, CircleShape),
-
-        )
-}
-
-@Composable
-private fun CircleButton(
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier,
-    colors: IconButtonColors = IconButtonDefaults.filledIconButtonColors(
-        containerColor = Color.White.copy(alpha = 0.1f)
-    ),
-    content: @Composable () -> Unit
-) {
-    FilledIconButton(
-        onClick = onClick,
-        modifier = modifier.size(72.dp),
-        shape = CircleShape,
-        colors = colors,
-        content = content
-    )
 }
---
+++ app/src/main/java/com/brainwallet/ui/screen/unlock/composable/CircleButton.kt
@@ -0,0 +1,29 @@
+package com.brainwallet.ui.screen.unlock.composable
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.IconButtonColors
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CircleButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    colors: IconButtonColors = IconButtonDefaults.filledIconButtonColors(
+        containerColor = Color.White.copy(alpha = 0.1f)
+    ),
+    content: @Composable () -> Unit
+) {
+    FilledIconButton(
+        onClick = onClick,
+        modifier = modifier.size(72.dp),
+        shape = CircleShape,
+        colors = colors,
+        content = content
+    )
+}
