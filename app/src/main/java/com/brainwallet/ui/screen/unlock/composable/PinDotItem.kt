b97d6103614af6fc354dc530520693fde9d774ea
andhikayuana <andhikayuana@gmail.com>
2025-02-10 14:47:40 +0700

    chore: move PinDotItem and CircleButton to com.brainwallet.ui.screen.unlock.composable

---
+++ app/src/main/java/com/brainwallet/ui/screen/unlock/composable/PinDotItem.kt
@@ -0,0 +1,27 @@
+package com.brainwallet.ui.screen.unlock.composable
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PinDotItem(
+    modifier: Modifier = Modifier,
+    checked: Boolean = false
+) {
+    Box(
+        modifier = modifier
+            .padding(horizontal = 4.dp)
+            .size(16.dp)
+            .background(if (checked) Color.White else Color.Transparent, CircleShape)
+            .border(1.dp, Color.White, CircleShape),
+
+        )
+}
