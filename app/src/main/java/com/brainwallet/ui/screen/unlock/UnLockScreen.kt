8586f65213b854836ee12819bbe43307d74d2958
andhikayuana <andhikayuana@gmail.com>
2025-02-10 14:40:28 +0700

    feat: UI UnLockScreen

--- app/src/main/java/com/brainwallet/ui/screen/unlock/UnLockScreen.kt
+++ app/src/main/java/com/brainwallet/ui/screen/unlock/UnLockScreen.kt
@@ -3,40 +3,62 @@
 package com.brainwallet.ui.screen.unlock
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonColors
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.brainwallet.BuildConfig
 import com.brainwallet.R
+import com.brainwallet.tools.util.BRConstants
 
 @Composable
 fun UnLockScreen(
     onEvent: (UnLockEvent) -> Unit,
     viewModel: UnLockViewModel = viewModel()
 ) {
+    val state by viewModel.state.collectAsState()
+
+    LaunchedEffect(state.pinDigits.all { it > -1 }) {
+        //
+    }
+
     /// Layout values
     val columnPadding = 18
     val horizontalVerticalSpacing = 8
@@ -61,42 +83,148 @@ fun UnLockScreen(
             modifier = Modifier
                 .padding(paddingValues)
                 .padding(columnPadding.dp)
-                .fillMaxSize(),
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(horizontalVerticalSpacing.dp),
         ) {
-
-            //todo
-
-            //logo
 
             Image(
                 painter = painterResource(R.drawable.brainwallet_logotype_white),
                 contentDescription = "logo"
             )
 
-            Spacer(modifier = Modifier.height(spacerHeight.dp))
+            Spacer(Modifier.height(24.dp))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .background(Color.White)
+            ) {
+                //todo
+                Text("todo")
+            }
+
+            Spacer(Modifier.height(16.dp))
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                state.pinDigits.forEach { digit ->
+                    PinDotItem(checked = digit > -1)
+                }
+            }
+
+            Spacer(Modifier.height(16.dp))
 
             FlowRow(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(horizontalVerticalSpacing.dp),
-                verticalArrangement = Arrangement.spacedBy(horizontalVerticalSpacing.dp),
+                horizontalArrangement = Arrangement.spacedBy(24.dp),
+                verticalArrangement = Arrangement.spacedBy(24.dp),
                 maxItemsInEachRow = maxItemsPerRow
             ) {
                 //pin number button
+
+                repeat(9) { index ->
+                    val number = index + 1
+                    CircleButton(
+                        onClick = {
+                            viewModel.onEvent(UnLockEvent.OnPinDigitChange(digit = number))
+                        },
+                    ) {
+                        Text(
+                            text = number.toString(),
+                            style = MaterialTheme.typography.headlineMedium,
+                            color = Color.White
+                        )
+                    }
+                }
+
+                // Bottom row with biometric, 0, and backspace
+                CircleButton(
+                    onClick = {
+                        //
+                    },
+                    colors = IconButtonDefaults.filledIconButtonColors(
+                        containerColor = Color.Transparent
+                    ),
+                ) {
+                    Icon(
+                        Icons.Default.Face,
+                        contentDescription = "Biometric",
+                        tint = Color.White
+                    )
+                }
+
+                CircleButton(
+                    onClick = {
+                        viewModel.onEvent(UnLockEvent.OnPinDigitChange(digit = 0))
+                    },
+                ) {
+                    Text(
+                        text = "0",
+                        style = MaterialTheme.typography.headlineMedium,
+                        color = Color.White
+                    )
+                }
+
+                CircleButton(
+                    onClick = {
+                        viewModel.onEvent(UnLockEvent.OnDeletePinDigit)
+                    },
+                    colors = IconButtonDefaults.filledIconButtonColors(
+                        containerColor = Color.Transparent
+                    ),
+                ) {
+                    Icon(
+                        Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Delete",
+                        tint = Color.White
+                    )
+                }
             }
 
             Spacer(modifier = Modifier.weight(1f))
 
             //app version
             Text(
-                text = "${BuildConfig.VERSION_CODE}",
+                text = BRConstants.APP_VERSION_NAME_CODE,
                 style = MaterialTheme.typography.bodyMedium.copy(textAlign = TextAlign.Center)
             )
         }
     }
 }
 
-private fun BuildConfig.appVersion(): String =
-    "${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})"
+
+@Composable
+private fun PinDotItem(
+    modifier: Modifier = Modifier,
+    checked: Boolean = false
+) {
+    Box(
+        modifier = modifier
+            .padding(horizontal = 4.dp)
+            .size(16.dp)
+            .background(if (checked) Color.White else Color.Transparent, CircleShape)
+            .border(1.dp, Color.White, CircleShape),
+
+        )
+}
+
+@Composable
+private fun CircleButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    colors: IconButtonColors = IconButtonDefaults.filledIconButtonColors(
+        containerColor = Color.White.copy(alpha = 0.1f)
+    ),
+    content: @Composable () -> Unit
+) {
+    FilledIconButton(
+        onClick = onClick,
+        modifier = modifier.size(72.dp),
+        shape = CircleShape,
+        colors = colors,
+        content = content
+    )
+}
