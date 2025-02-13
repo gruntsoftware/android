888fdf92e98383ee5fb4daac39e3b9ba4f5fb157
andhikayuana <andhikayuana@gmail.com>
2025-02-14 05:24:08 +0700

    feat: display currency inside UnLockScreen

--- app/src/main/java/com/brainwallet/ui/screen/unlock/UnLockState.kt
+++ app/src/main/java/com/brainwallet/ui/screen/unlock/UnLockState.kt
@@ -3,5 +3,8 @@ package com.brainwallet.ui.screen.unlock
 data class UnLockState(
     val pinDigits: List<Int> = List(6) { -1 },
     val biometricEnabled: Boolean = false,
+    val iso: String = "USD",
+    val formattedCurrency: String = ""
 )
 //todo
+
--- app/src/main/java/com/brainwallet/ui/screen/unlock/UnLockViewModel.kt
+++ app/src/main/java/com/brainwallet/ui/screen/unlock/UnLockViewModel.kt
@@ -1,10 +1,17 @@
 package com.brainwallet.ui.screen.unlock
 
 import androidx.lifecycle.ViewModel
+import com.brainwallet.tools.manager.BRSharedPrefs
+import com.brainwallet.tools.sqlite.CurrencyDataSource
+import com.brainwallet.tools.util.BRConstants
+import com.brainwallet.tools.util.BRCurrency
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import timber.log.Timber
+import java.math.BigDecimal
+
 
 class UnLockViewModel : ViewModel() {
 
@@ -39,6 +46,31 @@ class UnLockViewModel : ViewModel() {
             }
 
             UnLockEvent.OnBackClick -> Unit
+            is UnLockEvent.OnLoad -> {
+                val iso = BRSharedPrefs.getIsoSymbol(event.context)
+
+                var formattedCurrency: String? = null
+                val currency = CurrencyDataSource.getInstance(event.context).getCurrencyByIso(iso)
+                if (currency != null) {
+                    val roundedPriceAmount: BigDecimal =
+                        BigDecimal(currency.rate.toDouble()).multiply(BigDecimal(100))
+                            .divide(BigDecimal(100), 2, BRConstants.ROUNDING_MODE)
+                    formattedCurrency =
+                        BRCurrency.getFormattedCurrencyString(
+                            event.context,
+                            iso,
+                            roundedPriceAmount
+                        )
+                } else {
+                    Timber.w("The currency related to %s is NULL", iso)
+                }
+
+                if (formattedCurrency != null) {
+                    _state.update {
+                        it.copy(iso = iso, formattedCurrency = formattedCurrency)
+                    }
+                }
+            }
         }
     }
 }
