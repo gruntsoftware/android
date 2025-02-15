0b9a022aacb50d13ee44f8197b807ee53865801e
andhikayuana <andhikayuana@gmail.com>
2025-02-16 00:09:32 +0700

    feat: UnLockScreen using onNavigate

--- app/src/main/java/com/brainwallet/ui/screens/unlock/UnLockEvent.kt
+++ app/src/main/java/com/brainwallet/ui/screens/unlock/UnLockEvent.kt
@@ -5,7 +5,6 @@ import android.content.Context
 sealed class UnLockEvent {
     data class OnLoad(val context: Context) : UnLockEvent()
     data class OnPinDigitChange(val digit: Int) : UnLockEvent()
-    object OnBackClick : UnLockEvent()
     object OnDeletePinDigit : UnLockEvent()
 }
 
