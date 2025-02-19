5c96906918a7f9b245398ac55e5876006f7a2d6e
andhikayuana <andhikayuana@gmail.com>
2025-02-19 11:52:11 +0700

    chore: remove unused colors

--- app/src/main/java/com/brainwallet/ui/theme/Color.kt
+++ app/src/main/java/com/brainwallet/ui/theme/Color.kt
@@ -2,37 +2,6 @@ package com.brainwallet.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-//val primaryBackgroundLight = Color(0xFFFFFFFF)
-//val onPrimaryBackgroundLight = Color(0xFF0F0853)
-//val secondaryBackgroundLight = Color(0xFF402DAE)
-//val onSecondaryBackgroundLight = Color(0xFF0F0853)
-//val errorBackgroundLight = Color(0xFFFFFFFF)
-//val onErrorBackgroundLight = Color(0xFFCE3025)
-//val infoBackgroundLight = Color(0xFFFFFFFF)
-//val onInfoBackgroundLight = Color(0xFF2968F2)
-//val affirmBackgroundLight = Color(0xFFFFFFFF)
-//val onAffirmBackgroundLight = Color(0xFF25AA2C)
-//val warnBackgroundLight = Color(0xFFFFFFFF)
-//val onWarnBackgroundLight = Color(0xFFFFAE00)
-//val primaryBackgroundWhite = Color(0xFFFFFFFF)
-//val onPrimaryBackgroundWhite = Color(0xFF151515)
-//
-//val primaryBackgroundDark = Color(0xFF0F0853)
-//val onPrimaryBackgroundDark = Color(0xFFFFFFFF)
-//val secondaryBackgroundDark = Color(0xFF0F0853)
-//val onSecondaryBackgroundDark = Color(0xFF402DAE)
-//val errorBackgroundDark = Color(0xFF0F0853)
-//val onErrorBackgroundDark = Color(0xFFCE3025)
-//val infoBackgroundDark = Color(0xFF0F0853)
-//val onInfoBackgroundDark = Color(0xFFFFFFFF)
-//val affirmBackgroundDark = Color(0xFF2968F2)
-//val onAffirmBackgroundDark = Color(0xFF25AA2C)
-//val warnBackgroundDark = Color(0xFF0F0853)
-//val onWarnBackgroundDark = Color(0xFFCE3025)
-//val primaryBackgroundNearBlack= Color(0xFF151515)
-//val onPrimaryBackgroundNearBlack = Color(0xFFFFFFFF)
-
-
 val midnight = Color(0xFF0F0853)
 val grape = Color(0xFF402DAE)
 val cheddar = Color(0xFFFFAE00)
