f852b9507813241e829a5d3bace42da6af77d4c9
andhikayuana <andhikayuana@gmail.com>
2025-02-19 15:13:48 +0700

    chore: wip reset pin when disabled

--- app/src/main/java/com/brainwallet/navigation/LegacyNavigation.kt
+++ app/src/main/java/com/brainwallet/navigation/LegacyNavigation.kt
@@ -5,8 +5,10 @@ import android.content.Context
 import android.content.Intent
 import com.brainwallet.R
 import com.brainwallet.presenter.activities.BreadActivity
+import com.brainwallet.presenter.activities.LoginActivity
 import com.brainwallet.ui.BrainwalletActivity
 import timber.log.Timber
+
 
 //provide old navigation using intent activity
 object LegacyNavigation {
@@ -23,9 +25,12 @@ object LegacyNavigation {
         auth: Boolean
     ) {
         Timber.i("timber: startBreadActivity: %s", from.javaClass.name)
-        val intent = if (auth) BrainwalletActivity.createIntent(from, Route.UnLock)
-        else Intent(from, BreadActivity::class.java)
+//        val intent = if (auth) BrainwalletActivity.createIntent(from, Route.UnLock)
+//        else Intent(from, BreadActivity::class.java)
+        //TODO WIP HERE
 
+        val toStart: Class<*> = if (auth) LoginActivity::class.java else BreadActivity::class.java
+        val intent = Intent(from, toStart)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
         from.startActivity(intent)
         from.overridePendingTransition(R.anim.fade_up, R.anim.fade_down)
