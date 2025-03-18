7709c17a639e22145c56b9228d40994fd39a6a16
Kerry Washington <mrkerrywashington@icloud.com>
2025-03-18 11:03:16 +0000

    Added more security section titles
    added localizations

--- app/src/main/java/com/brainwallet/ui/screens/home/composable/settingsrows/SecurityDetail.kt
+++ app/src/main/java/com/brainwallet/ui/screens/home/composable/settingsrows/SecurityDetail.kt
@@ -1,9 +1,14 @@
 package com.brainwallet.ui.screens.home.composable.settingsrows
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -14,6 +19,9 @@ import com.brainwallet.R
 fun SecurityDetail(
     modifier: Modifier = Modifier
 ) {
+
+    /// Layout values
+    val contentHeight = 60
     val horizontalPadding = 14
 
     SettingRowItemExpandable(
@@ -21,9 +29,61 @@ fun SecurityDetail(
         title = stringResource(R.string.settings_title_security),
     ) {
         Column(
-            modifier = Modifier.padding(horizontal = horizontalPadding.dp)
+            modifier = Modifier.padding(horizontal = horizontalPadding.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text("TODO Content Here")
+
+            Row(
+                modifier = Modifier
+                    .height(contentHeight.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(stringResource(R.string.security_PIN_title))
+                Spacer(modifier = Modifier.weight(1f))
+                Button(onClick = {
+                }) {
+                    Text(stringResource(R.string.security_PIN_button))
+                }
+            }
+
+            Row(
+                modifier = Modifier
+                    .height(contentHeight.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(stringResource(R.string.security_seed_phrase_title))
+                Spacer(modifier = Modifier.weight(1f))
+                Button(onClick = {
+                }) {
+                    Text(stringResource(R.string.security_phrase_button))
+                }
+            }
+
+            Row(
+                modifier = Modifier
+                    .height(contentHeight.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(stringResource(R.string.security_brainwallet_phrase_title))
+                Spacer(modifier = Modifier.weight(1f))
+                Button(onClick = {
+                }) {
+                    Text(stringResource(R.string.security_phrase_button))
+                }
+            }
+            Row(
+                modifier = Modifier
+                    .height(contentHeight.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(stringResource(R.string.security_share_data_title))
+                Spacer(modifier = Modifier.weight(1f))
+                Button(onClick = {
+                }) {
+                    Text(stringResource(R.string.Button_yes))
+                }
+            }
+
         }
     }
 }
