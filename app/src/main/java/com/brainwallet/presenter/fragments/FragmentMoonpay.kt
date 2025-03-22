b509172c12921b7b5f680d3ee0fafda452a89d2d
Kerry Washington <mrkerrywashington@icloud.com>
2025-03-22 21:32:38 +0000

    removed the unused images
    Added the copy button

--- app/src/main/java/com/brainwallet/presenter/fragments/FragmentMoonpay.kt
+++ app/src/main/java/com/brainwallet/presenter/fragments/FragmentMoonpay.kt
@@ -1,5 +1,6 @@
 package com.brainwallet.presenter.fragments
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,9 +8,13 @@ import android.view.ViewGroup
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.brainwallet.R
+import com.brainwallet.tools.manager.BRClipboardManager
+import com.brainwallet.tools.manager.BRSharedPrefs
 
 class FragmentMoonpay : Fragment() {
 
@@ -24,8 +29,14 @@ class FragmentMoonpay : Fragment() {
             super.onViewCreated(view, savedInstanceState)
 
             val webView = view.findViewById<WebView>(R.id.web_view)
-            val btnClose = view.findViewById<Button>(R.id.close_button)
+            val buttonClose = view.findViewById<ImageButton>(R.id.close_button)
+            val copyAddressButton = view.findViewById<ImageButton>(R.id.copy_button)
+            var receiveAddressLabel = view.findViewById<TextView>(R.id.receive_address)
+            val addressText = BRSharedPrefs.getReceiveAddress(context)
 
+           // buttonClose
+            receiveAddressLabel.setText(addressText)
+            receiveAddressLabel.setTextColor(receiveAddressLabel.getResources().getColor(R.color.midnight, null));
             webView.setInitialScale(80)
             webView.settings.apply {
                 javaScriptEnabled = true
@@ -36,13 +47,19 @@ class FragmentMoonpay : Fragment() {
                 loadWithOverviewMode = true
             }
 
-            // Load a URL
             webView.webViewClient = WebViewClient()
             webView.loadUrl("https://www.brainwallet.co/mobile-top-up.html")
 
-            // Close button action
-            btnClose.setOnClickListener {
+            buttonClose.setOnClickListener {
                 parentFragmentManager.popBackStack()
+            }
+
+            receiveAddressLabel.setOnClickListener {
+                BRClipboardManager.putClipboard(context, receiveAddressLabel.getText().toString())
+            }
+
+            copyAddressButton.setOnClickListener {
+                BRClipboardManager.putClipboard(context, receiveAddressLabel.getText().toString())
             }
         }
     }
