b3c26c5f7b3fa53220b0dc2744cb0c157d5cfec4
andhikayuana <andhikayuana@gmail.com>
2025-05-29 14:35:57 +0700

    fix: android: Footer version label is obfuscated

--- app/src/main/java/com/brainwallet/ui/screens/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/brainwallet/ui/screens/welcome/WelcomeScreen.kt
@@ -12,7 +12,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -89,7 +91,8 @@ fun WelcomeScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(BrainwalletTheme.colors.surface),
+            .background(BrainwalletTheme.colors.surface)
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween
     ) {
