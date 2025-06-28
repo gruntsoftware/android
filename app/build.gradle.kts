1d310110b635a7fce0d78ec42fb9710f3d6deb24
Kerry Washington <mrkerrywashington@icloud.com>
2025-06-28 10:34:22 +0100

    code bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "ltd.grunt.brainwallet"
         minSdk = 29
         targetSdk = 34
-        versionCode = 202506272
+        versionCode = 202506281
         versionName = "v4.6.2"
 
         multiDexEnabled = true
