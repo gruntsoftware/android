4ff50b82fcd882439a3b24f859233320668dfa81
Andhika Yuana <andhikayuana@gmail.com>
2025-06-29 13:50:10 +0700

    fix: fix YourSeedProveItState.isWordUsedCorrectly (#111)

--- app/src/main/java/com/brainwallet/ui/screens/yourseedproveit/YourSeedProveItState.kt
+++ app/src/main/java/com/brainwallet/ui/screens/yourseedproveit/YourSeedProveItState.kt
@@ -2,7 +2,7 @@ package com.brainwallet.ui.screens.yourseedproveit
 
 data class YourSeedProveItState(
     val correctSeedWords: Map<Int, SeedWordItem> = emptyMap(),
-    val shuffledSeedWords: List<String> = emptyList(),
+    val shuffledSeedWords: List<Pair<Int, String>> = emptyList(),
     val orderCorrected: Boolean = false,
 )
 
@@ -10,3 +10,9 @@ data class SeedWordItem(
     val expected: String,
     val actual: String = ""
 )
+
+fun YourSeedProveItState.isWordUsedCorrectly(currentIndex: Int, currentWord: String): Boolean {
+    return correctSeedWords[currentIndex]?.let { seedWordItem ->
+        seedWordItem.expected == currentWord && seedWordItem.actual == currentWord
+    } ?: false
+}
