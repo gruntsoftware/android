d5276ac494890ec47942852b069363df1d69d0d2
Joseph Sanjaya <sanjayajosep@gmail.com>
2025-08-25 21:40:01 +0700

    fix(#226): android test failure on RecoverWalletScreenGrabsTest
    Move SCREENGRAB_PAPERKEY build config to debug build type
    
    The `SCREENGRAB_PAPERKEY` build configuration field, used for screengrab tests, was previously defined in both the `debug` and `screengrab` build types.
    
    This commit moves the definition solely to the `debug` build type, as the `screengrab` build type inherits from `debug` and thus already has access to this field.
    
    Additionally, unused imports were removed from `BrainwalletScreengrabApp.kt`.

--- app/src/screengrab/kotlin/com/brainwallet/BrainwalletScreengrabApp.kt
+++ app/src/screengrab/kotlin/com/brainwallet/BrainwalletScreengrabApp.kt
@@ -1,9 +1,6 @@
 package com.brainwallet
 
 import com.brainwallet.data.source.RemoteConfigSource
-import com.brainwallet.di.appModule
-import com.brainwallet.di.dataModule
-import com.brainwallet.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
