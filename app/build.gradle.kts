b37c385f261a654709d9f1765a4cd3ce1fc93309
Joseph Sanjaya <sanjayajosep@gmail.com>
2025-09-20 17:27:29 +0700

    chore: Remove ktlint from project
    This commit removes the ktlint configuration and associated tasks from the `app/build.gradle.kts` file. It also removes the `pinterest-ktlint` dependency from the `gradle/libs.versions.toml` file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -182,15 +182,7 @@ android {
     //TODO: rename output apk/bundle
 }
 
-val ktlint by configurations.creating
-
 dependencies {
-    ktlint(libs.pinterest.ktlint) {
-        attributes {
-            attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
-        }
-    }
-
     val gamesModule = findProject(":modules:games:content")
     if (gamesModule != null) {
         implementation(gamesModule)
@@ -256,38 +248,6 @@ dependencies {
     androidTestImplementation(libs.bundles.android.test)
     androidTestImplementation(libs.fastlane.screengrab)
     androidTestImplementation(libs.slf4j.android)
-}
-
-val ktlintCheck by tasks.registering(JavaExec::class) {
-    group = LifecycleBasePlugin.VERIFICATION_GROUP
-    description = "Check Kotlin code style"
-    classpath = ktlint
-    mainClass.set("com.pinterest.ktlint.Main")
-    // see https://pinterest.github.io/ktlint/install/cli/#command-line-usage for more information
-    args(
-        "**/src/**/*.kt",
-        "**.kts",
-        "!**/build/**",
-    )
-}
-
-tasks.check {
-    dependsOn(ktlintCheck)
-}
-
-tasks.register<JavaExec>("ktlintFormat") {
-    group = LifecycleBasePlugin.VERIFICATION_GROUP
-    description = "Check Kotlin code style and format"
-    classpath = ktlint
-    mainClass.set("com.pinterest.ktlint.Main")
-    jvmArgs("--add-opens=java.base/java.lang=ALL-UNNAMED")
-    // see https://pinterest.github.io/ktlint/install/cli/#command-line-usage for more information
-    args(
-        "-F",
-        "**/src/**/*.kt",
-        "**.kts",
-        "!**/build/**",
-    )
 }
 
 tasks.withType<Test> {
