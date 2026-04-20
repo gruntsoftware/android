879ab6fd11ffe882d5deaff73868562b9199fa71
Kerry Washington <mrkerrywashington@icloud.com>
2026-04-20 09:27:54 +0100

    Moved clipboard test point into event (#207)
    * Moved clipboard test point into event
    
    * renamed

--- app/src/main/java/com/brainwallet/ui/screens/main/MainViewModel.kt
+++ app/src/main/java/com/brainwallet/ui/screens/main/MainViewModel.kt
@@ -1,5 +1,9 @@
 package com.brainwallet.ui.screens.main
 import android.app.Application
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.viewModelScope
 import com.brainwallet.R
 import com.brainwallet.constants.BWConstants
@@ -11,7 +15,6 @@ import com.brainwallet.data.repository.LtcRepository
 import com.brainwallet.data.repository.SettingRepository
 import com.brainwallet.data.repository.TxRepository
 import com.brainwallet.tools.manager.AnalyticsManager
-import com.brainwallet.tools.manager.BRClipboardManager
 import com.brainwallet.tools.manager.BRSharedPrefs
 import com.brainwallet.tools.sqlite.TransactionDataSource
 import com.brainwallet.tools.util.BRExchange.ONE_LITECOIN_OF_LITOSHIS
@@ -436,7 +439,15 @@ class MainViewModel(
                     Blockchain browser URL: $txIDBrowserURL
                 """.trimIndent()
 
-                BRClipboardManager.putDetailsClipboard(app, transactionDetailsString)
+                try {
+                    val clipboard = app.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                    val clip = ClipData
+                        .newPlainText("Transaction Details: ", transactionDetailsString)
+                    clipboard.setPrimaryClip(clip)
+                    AnalyticsManager.logCustomEventWithParams("txn_details_copied", null)
+                } catch (e: java.lang.Exception) {
+                    Timber.e(e)
+                }
             }
         }
     }
