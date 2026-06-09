8bf0c0dae2b6f97513d9ee888eefe1930e71767f
kcw-grunt <mrkerrywashington@icloud.com>
2026-06-09 21:33:53 +0100

    WIP

---
+++ app/src/main/java/com/brainwallet/ui/screens/gamehub/fallinmoji/GdxFallinmojiGameFragment.kt
@@ -0,0 +1,23 @@
+package com.brainwallet.ui.screens.gamehub.fallinmoji
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
+import com.badlogic.gdx.backends.android.AndroidFragmentApplication
+
+class GdxFallinmojiGameFragment : AndroidFragmentApplication() {
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View {
+            val config = AndroidApplicationConfiguration().apply {
+                useImmersiveMode = true
+                useGL30 = false        // match what your game targets
+            }
+            // Swap in your actual ApplicationListener / Game subclass:
+            return initializeForView(FallinmojiGame(), config)
+        }
+    }
+}
